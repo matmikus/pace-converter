5a050460461b640797c5674d36ea771b987cdc7e
mmikolajczak <mmikolajczak@MMIKOLAJCZAK-MBP16.local>
2025-03-24 13:06:12 +0100

    fix

--- src/components/coffee.tsx
+++ src/components/coffee.tsx
@@ -1,33 +1,33 @@
 export default function Coffee () {
     return (
-        <div style={{ width: '100%', 'max-width': '400px', 'font-family': 'Arial, sans-serif' }}>
+        <div style={{ width: '100%', 'maxWidth': '400px', 'fontFamily': 'Arial, sans-serif' }}>
             <div style={{
-                'box-sizing': 'border-box',
-                'background-color': '#FFFFFF',
+                'boxSizing': 'border-box',
+                'backgroundColor': '#FFFFFF',
                 padding: '11px',
-                'border-radius': '16px',
-                'box-shadow': '0 0 rgba(0,0,0,0), 0 0 rgba(0,0,0,0), 0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -4px rgba(0, 0, 0, 0.1)',
+                'borderRadius': '16px',
+                'boxShadow': '0 0 rgba(0,0,0,0), 0 0 rgba(0,0,0,0), 0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -4px rgba(0, 0, 0, 0.1)',
                 width: '100%',
                 color: '#000000DE',
-                'text-align': 'center'
+                'textAlign': 'center'
             }}>
-                <h3 style={{ 'font-size': '13px', 'font-weight': '600', 'line-height': '1.5', margin: '0' }}> Postaw
+                <h3 style={{ 'fontSize': '13px', 'fontWeight': '600', 'lineHeight': '1.5', margin: '0' }}> Postaw
                     kawę za: </h3></div>
-            <div style={{ display: 'flex', gap: '8px', 'margin-top': '12px', 'justify-content': 'center' }}>
+            <div style={{ display: 'flex', gap: '8px', 'marginTop': '12px', 'justifyContent': 'center' }}>
                 <a href="https://buycoffee.to/matmikus?coffeeSize=small"
                    style={{
                        display: 'flex',
-                       'flex-direction': 'column',
-                       'justify-content': 'center',
-                       'align-items': 'center',
+                       'flexDirection': 'column',
+                       'justifyContent': 'center',
+                       'alignItems': 'center',
                        padding: '11px',
-                       'border-radius': '16px',
-                       'box-shadow': '0 0 rgba(0,0,0,0), 0 0 rgba(0,0,0,0), 0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -4px rgba(0, 0, 0, 0.1)',
+                       'borderRadius': '16px',
+                       'boxShadow': '0 0 rgba(0,0,0,0), 0 0 rgba(0,0,0,0), 0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -4px rgba(0, 0, 0, 0.1)',
                        width: '100%',
-                       'text-align': 'center',
-                       'background-color': '#FFFFFF',
+                       'textAlign': 'center',
+                       'backgroundColor': '#FFFFFF',
                        color: '#000000DE',
-                       'text-decoration': 'none'
+                       'textDecoration': 'none'
                    }}
                    target="_blank"
                    title="Postaw kawę za: 5 zł"> < img
@@ -35,26 +35,26 @@ export default function Coffee () {
                     alt="small coffee icon"
                     style={{ width: '28px', height: '28px' }} />
                     <span style={{
-                        'font-size': '13px',
-                        'font-weight': '700',
-                        'margin-top': '8px',
-                        'line-height': '1.5'
+                        'fontSize': '13px',
+                        'fontWeight': '700',
+                        'marginTop': '8px',
+                        'lineHeight': '1.5'
                     }}> 5&nbsp;zł </span>
                 </a>
                 <a href="https://buycoffee.to/matmikus?coffeeSize=medium"
                    style={{
                        display: 'flex',
-                       'flex-direction': 'column',
-                       'justify-content': 'center',
-                       'align-items': 'center',
+                       'flexDirection': 'column',
+                       'justifyContent': 'center',
+                       'alignItems': 'center',
                        'padding': '11px',
-                       'border-radius': '16px',
-                       'box-shadow': '0 0 rgba(0,0,0,0), 0 0 rgba(0,0,0,0), 0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -4px rgba(0, 0, 0, 0.1)',
+                       'borderRadius': '16px',
+                       'boxShadow': '0 0 rgba(0,0,0,0), 0 0 rgba(0,0,0,0), 0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -4px rgba(0, 0, 0, 0.1)',
                        width: '100%',
-                       'text-align': 'center',
-                       'background-color': '#FFFFFF',
+                       'textAlign': 'center',
+                       'backgroundColor': '#FFFFFF',
                        color: '#000000DE',
-                       'text-decoration': 'none'
+                       'textDecoration': 'none'
                    }}
                    target="_blank"
                    title="Postaw kawę za: 10 zł"> < img
@@ -62,25 +62,25 @@ export default function Coffee () {
                     alt="medium coffee icon"
                     style={{ width: '28px', height: '28px' }} />
                     <span style={{
-                        'font-size': '13px',
-                        'font-weight': '700',
-                        'margin-top': '8px',
-                        'line-height': '1.5'
+                        'fontSize': '13px',
+                        'fontWeight': '700',
+                        'marginTop': '8px',
+                        'lineHeight': '1.5'
                     }}> 10&nbsp;zł </span></a>
                 <a href="https://buycoffee.to/matmikus?coffeeSize=large"
                    style={{
                        display: 'flex',
-                       'flex-direction': 'column',
-                       'justify-content': 'center',
-                       'align-items': 'center',
+                       'flexDirection': 'column',
+                       'justifyContent': 'center',
+                       'alignItems': 'center',
                        'padding': '11px',
-                       'border-radius': '16px',
-                       'box-shadow': '0 0 rgba(0,0,0,0), 0 0 rgba(0,0,0,0), 0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -4px rgba(0, 0, 0, 0.1)',
+                       'borderRadius': '16px',
+                       'boxShadow': '0 0 rgba(0,0,0,0), 0 0 rgba(0,0,0,0), 0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -4px rgba(0, 0, 0, 0.1)',
                        'width': '100%',
-                       'text-align': 'center',
-                       'background-color': '#FFFFFF',
+                       'textAlign': 'center',
+                       'backgroundColor': '#FFFFFF',
                        'color': '#000000DE',
-                       'text-decoration': 'none'
+                       'textDecoration': 'none'
                    }}
                    target="_blank"
                    title="Postaw kawę za: 15 zł"> < img
@@ -88,10 +88,10 @@ export default function Coffee () {
                     alt="large coffee icon"
                     style={{ width: '28px', height: '28px' }} />
                     <span style={{
-                        'font-size': '13px',
-                        'font-weight': '700',
-                        'margin-top': '8px',
-                        'line-height': '1.5'
+                        'fontSize': '13px',
+                        'fontWeight': '700',
+                        'marginTop': '8px',
+                        'lineHeight': '1.5'
                     }}> 15&nbsp;zł </span>
                 </a></div>
         </div>
